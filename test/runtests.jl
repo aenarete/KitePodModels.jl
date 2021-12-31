3af72101254ae4d272ba5433f06e34ddaf6c2ca2
Uwe Fechner <fechner@aenarete.eu>
2021-12-31 01:13:48 +0100

    Implemented first tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,16 @@
 using KitePodSimulator
 using Test
 
+cd("..")
+
 @testset "KitePodSimulator.jl" begin
-    # Write your tests here.
+    init_kcu()
+    @test get_depower() ≈ 0.236
+    @test get_steering() ≈ 0.0
+    set_depower_steering(1.0, 1.0)
+    on_timer()
+    # println("depower: ", get_depower())
+    # println("steering: ", get_steering())
+    @test get_depower() ≈ 0.23975
+    @test get_steering() ≈ 0.01
 end
