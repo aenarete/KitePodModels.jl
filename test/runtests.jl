ff4137aff545e9ff01f1de19b8db422ff8acbb02
Uwe Fechner <fechner@aenarete.eu>
2022-01-01 15:33:33 +0100

    use new interface, create step response plot during testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,16 @@
-using KitePodSimulator
+using KitePodSimulator, KiteUtils
 using Test
 
-cd("..")
+include("plot_step_response.jl")
 
 @testset "KitePodSimulator.jl" begin
-    init_kcu()
-    @test get_depower() ≈ 0.236
-    @test get_steering() ≈ 0.0
-    set_depower_steering(1.0, 1.0)
-    on_timer()
-    # println("depower: ", get_depower())
-    # println("steering: ", get_steering())
-    @test get_depower() ≈ 0.23975
-    @test get_steering() ≈ 0.01
+    kcu = KCUState()
+    init_kcu(kcu, se())
+    @test get_depower(kcu) ≈ 0.236
+    @test get_steering(kcu) ≈ 0.0
+    set_depower_steering(kcu, 1.0, 1.0)
+    on_timer(kcu)
+    @test get_depower(kcu) ≈ 0.23975
+    @test get_steering(kcu) ≈ 0.01
 end
+
