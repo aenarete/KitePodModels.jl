f19fda5d2641ed800b21a5ba6a056386f09941b0
Uwe Fechner <fechner@aenarete.eu>
2022-01-01 15:32:43 +0100

    remove all global variables

--- src/KitePodSimulator.jl
+++ src/KitePodSimulator.jl
@@ -5,51 +5,66 @@ module KitePodSimulator
 using KiteUtils, Parameters
 
 export calc_alpha_depower, init_kcu, set_depower_steering, get_depower, get_steering, on_timer
+export KCUState
 
 if ! isfile("./data/settings.yaml")
     copy_settings()
-end
-const set = se()              
+end   
 
-@with_kw mutable struct KCUState{S}
-    set_depower::S =         set.depower_offset * 0.01
-    set_steering::S =        0.0
-    depower::S =             set.depower_offset * 0.01   #    0 .. 1.0
-    steering::S =            0.0                         # -1.0 .. 1.0
+@with_kw mutable struct KCUState
+    set_depower::Float64 =         0.0
+    set_steering::Float64 =        0.0
+    depower::Float64 =             0.0                         #    0 .. 1.0
+    steering::Float64 =            0.0                         # -1.0 .. 1.0
+    set::Settings = se()
 end
 
-const kcu_state = KCUState{Float64}()
+"""
+    init_kcu(kcu::KCUState, set::Settings)
+
+Inititalze the simulator. Must be called at the beginning of the simulation. The 
+actual and the set values of depower are initialized to set.depower_offset * 0.01,
+the actual and the set values of steering to zero. 
+"""
+function init_kcu(kcu::KCUState, set::Settings)
+    kcu.set = set
+    kcu.set_depower =         set.depower_offset * 0.01
+    kcu.set_steering =        0.0
+    kcu.depower =             set.depower_offset * 0.01   #    0 .. 1.0
+    kcu.steering =            0.0                         # -1.0 .. 1.0
+    nothing
+end
 
 # Calculate the length increase of the depower line [m] as function of the relative depower
 # setting [0..1].
-function calc_delta_l(rel_depower)
-    u = set.depower_drum_diameter
+function calc_delta_l(kcu::KCUState, rel_depower)
+    u = kcu.set.depower_drum_diameter
     l_ro = 0.0
-    rotations = (rel_depower - 0.01 * set.depower_offset) * 10.0 * 11.0 / 3.0 * (3918.8 - 230.8) / 4096.
+    rotations = (rel_depower - 0.01 * kcu.set.depower_offset) * 10.0 * 11.0 / 3.0 * (3918.8 - 230.8) / 4096.
     while rotations > 0.0
          l_ro += u * π    
          rotations -= 1.0
-         u -= set.tape_thickness
+         u -= kcu.set.tape_thickness
     end
     if rotations < 0.0
-         l_ro += (-(u + set.tape_thickness) * rotations + u * (rotations + 1.0)) * π * rotations
+         l_ro += (-(u + kcu.set.tape_thickness) * rotations + u * (rotations + 1.0)) * π * rotations
     end
     return l_ro
 end
 
 
 """
-    calc_alpha_depower(rel_depower)
+    calc_alpha_depower(kcu::KCUState, rel_depower)
 
 Calculate the change of the angle between the kite and the last tether segment [rad] as function of the
 actual rel_depower value. 
 
 Returns `nothing` in case of error.
 """
-function calc_alpha_depower(rel_depower)
-    a   =  set.power2steer_dist
-    b_0 = set.height_b + 0.5 * set.height_k
-    b = b_0 + 0.5 * calc_delta_l(rel_depower) # factor 0.5 due to the pulleys
+function calc_alpha_depower(kcu::KCUState, rel_depower)
+    a   =  kcu.set.power2steer_dist
+    b_0 = kcu.set.height_b + 0.5 * kcu.set.height_k
+    b = b_0 + 0.5 * calc_delta_l(kcu, rel_depower) # factor 0.5 due to the pulleys
 
     c = sqrt(a * a + b_0 * b_0)
     # print 'a, b, c:', a, b, c, rel_depower
@@ -70,72 +85,61 @@ function calc_alpha_depower(rel_depower)
 end
 
 """
-    init_kcu()
-
-Inititalze the simulator. Must be called at the beginning of the simulation. The 
-actual and the set values of depower are initialized to set.depower_offset * 0.01,
-the actual and the set values of steering to zero. 
-"""
-function init_kcu()
-    kcu_state.set_depower =         set.depower_offset * 0.01
-    kcu_state.set_steering =        0.0
-    kcu_state.depower =             set.depower_offset * 0.01   #    0 .. 1.0
-    kcu_state.steering =            0.0                         # -1.0 .. 1.0
-    nothing
-end
-
-"""
-    set_depower_steering(depower, steering)
+    set_depower_steering(kcu::KCUState, depower, steering)
 
 Set the values of depower and steering. The value for depower must be between 0.0 and 1.0,
 the value for steering between -1.0 and +1.0 .
 """
-function set_depower_steering(depower, steering)
-    kcu_state.set_depower  = depower
-    kcu_state.set_steering = steering
+function set_depower_steering(kcu::KCUState, depower, steering)
+    kcu.set_depower  = depower
+    kcu.set_steering = steering
     nothing
 end
 
 """
-    get_depower()
+    get_depower(kcu::KCUState)
 
 Read the current depower value. Result will be between 0.0 and 1.0.
 """
-function get_depower();  return kcu_state.depower;  end
+function get_depower(kcu::KCUState);  return kcu.depower;  end
 
 """
-    get_steering()
+    get_steering(kcu::KCUState)
 
 Read the current depower value. Result will be between -1.0 and 1.0.
 """
-function get_steering(); return kcu_state.steering; end
+function get_steering(kcu::KCUState); return kcu.steering; end
 
 """
-    on_timer(dt = 1.0 / set.sample_freq)
+    on_timer(kcu::KCUState, dt = 0.0)
 
 Must be called at each clock tick. Parameter: Δt in seconds    
 Updates the current values of steering and depower depending
 on the set values and the last value.
+If dt == 0.0, then it will be set to 1.0 / kcu.set.sample_freq .
 """
-function on_timer(dt = 1.0 / set.sample_freq)
+function on_timer(kcu::KCUState, dt = 0.0)
+    if dt == 0.0
+        dt = 1.0 / kcu.set.sample_freq
+    end
     # calculate the depower motor velocity
-    vel_depower = (kcu_state.set_depower - kcu_state.depower) * set.depower_gain
+    vel_depower = (kcu.set_depower - kcu.depower) * kcu.set.depower_gain
     # println("vel_depower: $(vel_depower)")
-    if vel_depower > set.v_depower
-        vel_depower = set.v_depower
-    elseif vel_depower < -set.v_depower
-        vel_depower = -set.v_depower
+    if vel_depower > kcu.set.v_depower
+        vel_depower = kcu.set.v_depower
+    elseif vel_depower < -kcu.set.v_depower
+        vel_depower = -kcu.set.v_depower
     end
     # update the position
-    kcu_state.depower += vel_depower * dt
+    kcu.depower += vel_depower * dt
     # calculate the steering motor velocity
-    vel_steering = (kcu_state.set_steering - kcu_state.steering) * set.steering_gain
-    if vel_steering > set.v_steering
-        vel_steering = set.v_steering
-    elseif vel_steering < -set.v_steering
-        vel_steering = -set.v_steering
+    vel_steering = (kcu.set_steering - kcu.steering) * kcu.set.steering_gain
+    if vel_steering > kcu.set.v_steering
+        vel_steering = kcu.set.v_steering
+    elseif vel_steering < -kcu.set.v_steering
+        vel_steering = -kcu.set.v_steering
     end
-    kcu_state.steering += vel_steering * dt
+    kcu.steering += vel_steering * dt
     nothing
 end
 
--- test/plot_step_response.jl
+++ test/plot_step_response.jl
@@ -1,9 +1,12 @@
 using KitePodSimulator, KiteUtils, Plots
 
+cd("..")
+
 const t_end = 10.0 # simulation time
 const dt = 1.0 / se().sample_freq
 
-init_kcu()
+kcu = KCUState()
+init_kcu(kcu, se())
 
 const times = Float64[]
 const depower = Float64[]
@@ -14,18 +17,18 @@ const rel_depower_offset = se().depower_offset/100.0
 for step in 1:Int(round(t_end/dt))
     time = step * dt
     if step < 20
-        set_depower_steering(rel_depower_offset, 0.0)
+        set_depower_steering(kcu, rel_depower_offset, 0.0)
         push!(depower_set, rel_depower_offset)
         push!(steering_set, 0.0)
     else
-        set_depower_steering(rel_depower_offset+0.5, 0.5)
+        set_depower_steering(kcu, rel_depower_offset+0.5, 0.5)
         push!(depower_set, rel_depower_offset+0.5)
         push!(steering_set, 0.5)
     end
     push!(times, time)
-    on_timer()
-    push!(depower, get_depower())
-    push!(steering, get_steering())
+    on_timer(kcu)
+    push!(depower, get_depower(kcu))
+    push!(steering, get_steering(kcu))
 end
 l = @layout([a; b])
 plot(times, depower, layout=l, subplot=1, label = "depower", legend = :bottomright)
@@ -33,3 +36,4 @@ plot!(times, depower_set, layout=l, subplot=1, label  = "depower_set")
 plot!(times, steering, layout=l, subplot=2, label = "steering", legend = :bottomright)
 plot!(times, steering_set, layout=l, subplot=2, label  = "steering_set")
 savefig("docs/src/step_response.png")
+println("Saved step response in docs/src/step_response.png !")
