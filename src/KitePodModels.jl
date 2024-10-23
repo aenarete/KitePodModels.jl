41e36cdfebb66928792883d1a965f7662f104c57
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-10-23 13:22:31 +0200

    use set.cs_4p

--- src/KitePodModels.jl
+++ src/KitePodModels.jl
@@ -120,11 +120,12 @@ end
     set_depower_steering(kcu::KCU, depower, steering)
 
 Set the values of depower and steering. The value for depower must be between 0.0 and 1.0,
-the value for steering between -1.0 and +1.0 .
+the value for steering between -1.0 and +1.0 . The steering value is multiplied by the 
+constant kcu.set.cs_4p.
 """
 function set_depower_steering(kcu::KCU, depower, steering)
     kcu.set_depower  = depower
-    kcu.set_steering = steering
+    kcu.set_steering = steering * kcu.set.cs_4p
     nothing
 end
 
